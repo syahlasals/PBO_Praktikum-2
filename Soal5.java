b2ab320b483d76bdc8fbdb873f00dcc21f565252
syahla <112297591+syahlasals@users.noreply.github.com>
2024-08-30 15:32:21 +0700

    Update Soal5.java

--- Soal5.java
+++ Soal5.java
@@ -1,37 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ASUS
- */
 import java.util.Scanner;
 
 public class Soal5 {
-    // function compare to lexicography
-
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        String A = scan.nextLine();
-        String B = scan.nextLine();
-
-        int totalLength = A.length() + B.length();
-        System.out.println(totalLength);
-
-        if (A.compareTo(B) < 0) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Kata pertama : ");
+        String kataPertama = scanner.nextLine();
+        System.out.print("Kata kedua : ");
+        String kataKedua = scanner.nextLine();
+        
+        int panjangkata = kataPertama.length() + kataKedua.length();
+        System.out.println(panjangkata);
+        
+        if (kataPertama.compareTo(kataKedua) > 0) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
-
-        String capitalizedA = A.substring(0, 1).toUpperCase() + A.substring(1);
-        String capitalizedB = B.substring(0, 1).toUpperCase() + B.substring(1);
-        System.out.println(capitalizedA + " " + capitalizedB);
-
-        scan.close();
+        
+        String hasilKataPertama = kataPertama.substring(0, 1).toUpperCase() + kataPertama.substring(1);
+        String hasilkataKedua = kataKedua.substring(0, 1).toUpperCase() + kataKedua.substring(1);
+        System.out.println(hasilKataPertama + " " + hasilkataKedua);
+        
+        scanner.close();
     }
 }
